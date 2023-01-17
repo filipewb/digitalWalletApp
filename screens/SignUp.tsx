db0365d8dbe8533de4800c6409145f7e7ddf8f4d
Filipe Wolf Boeck <filipewolfboeck@MacBookAir.local>
2023-01-17 08:28:29 -0300

    insercao novos components screen signup

--- screens/SignUp.tsx
+++ screens/SignUp.tsx
@@ -17,6 +17,38 @@ import LinearGradient from 'react-native-linear-gradient';
 import {COLORS, SIZES, FONTS, icons, images} from '../constants';
 
 const SignUp = () => {
+
+  const [showPassword, setShowPassword] = React.useState(false)
+
+  const [areas, setAreas] = React.useState([])
+  const [selectedArea, setSelectedArea] = React.useState(null)
+  const [modalVisible, setModalVisible] = React.useState(false)
+
+  React.useEffect(() => {
+    fetch("https>//restcountries.eu/rest/v2/all")
+    .then(response => response.json())
+    .then(data => {
+      let areaData = data.map(item => {
+        return {
+          code: item.alpha2Code,
+          name: item.name,
+          callingCode: `+${item.callingCodes[0]}`,
+          flag: `https://www.countryflags.io/${item.alpha2Code}/flat/64.png`
+        }
+      })
+
+      setAreas(areaData)
+
+      if(areaData.length > 0) {
+        let defaultData = areaData.filter(a => a.code =="US")
+
+        if(defaultData.length > 0) {
+          setSelectedArea(defaultData[0])
+        }
+      }
+    })
+  }, [])
+
   function renderHeader() {
     return (
       <TouchableOpacity
@@ -71,33 +103,151 @@ const SignUp = () => {
 
   function renderForm() {
     return (
-        <View
+      <View
         style={{
-            marginTop: SIZES.padding * 3,
-            marginHorizontal: SIZES.padding * 3,
-        }}
-        >
-            <View style={{ marginTop: SIZES.padding * 3 }}>
-                <Text style={{ color: COLORS.lightGreen, /*...FONTS.body3*/ }}>
-                    Full Name
-                </Text>
-                <TextInput
-                    style={{
-                        marginVertical: SIZES.padding,
-                        borderBottomColor: COLORS.white,
-                        borderBottomWidth: 1,
-                        height: 40,
-                        color: COLORS.white,
-                        //...FONTS.h3
-                    }}
-                    placeholder="Enter Full Name"
-                    placeholderTextColor={COLORS.white}
-                    selectionColor={COLORS.white}
-                />
-            </View>
-
+          marginTop: SIZES.padding * 3,
+          marginHorizontal: SIZES.padding * 3,
+        }}>
+        <View style={{marginTop: SIZES.padding * 3}}>
+          <Text style={{color: COLORS.lightGreen /*...FONTS.body3*/}}>
+            Full Name
+          </Text>
+          <TextInput
+            style={{
+              marginVertical: SIZES.padding,
+              borderBottomColor: COLORS.white,
+              borderBottomWidth: 1,
+              height: 40,
+              color: COLORS.white,
+              //...FONTS.h3
+            }}
+            placeholder="Enter Full Name"
+            placeholderTextColor={COLORS.white}
+            selectionColor={COLORS.white}
+          />
         </View>
-    )
+        <View style={{marginTop: SIZES.padding * 2}}>
+          <Text style={{color: COLORS.lightGreen /* ...FONTS.body3 */}}>
+            Phone Number
+          </Text>
+
+          <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity
+              style={{
+                width: 100,
+                height: 50,
+                marginHorizontal: 5,
+                borderBottomColor: COLORS.white,
+                borderBottomWidth: 1,
+                flexDirection: 'row',
+                // ...FONTS.body2
+              }}
+              onPress={() => console.log('Show modal')}>
+              <View style={{justifyContent: 'center'}}>
+                <Image
+                  source={icons.down}
+                  style={{
+                    width: 10,
+                    height: 10,
+                    tintColor: COLORS.white,
+                  }}
+                />
+              </View>
+              <View style={{justifyContent: 'center', marginLeft: 5}}>
+                <Image
+                  source={{ uri: selectedArea?.flag }}
+                  resizeMode="contain"
+                  style={{
+                    width: 30,
+                    height: 30,
+                  }}
+                />
+              </View>
+
+              <View style={{justifyContent: 'center', marginLeft: 5}}>
+                <Text style={{color: COLORS.white /* ...FONTS.body3 */}}>
+                  US+1
+                </Text>
+              </View>
+            </TouchableOpacity>
+
+            <TextInput
+              style={{
+                flex: 1,
+                marginVertical: SIZES.padding,
+                borderBottomColor: COLORS.white,
+                borderBottomWidth: 1,
+                height: 40,
+                color: COLORS.white,
+                // ...FONTS.body3
+              }}
+              placeholder="Enter Phone Number"
+              placeholderTextColor={COLORS.white}
+              selectionColor={COLORS.white}
+            />
+          </View>
+        </View>
+
+        <View style={{marginTop: SIZES.padding * 2}}>
+          <Text style={{color: COLORS.lightGreen /* ...FONTS.body3 */}}>
+            Password
+          </Text>
+          <TextInput
+            style={{
+              marginVertical: SIZES.padding,
+              borderBottomColor: COLORS.white,
+              borderBottomWidth: 1,
+              height: 40,
+              color: COLORS.white,
+              // ...FONTS.body3
+            }}
+            placeholder="Enter Password"
+            placeholderTextColor={COLORS.white}
+            selectionColor={COLORS.white}
+            secureTextEntry={!showPassword}
+          />
+          <TouchableOpacity
+            style={{
+              position: 'absolute',
+              right: 0,
+              bottom: 10,
+              height: 30,
+              width: 30,
+            }}
+            onPress={() => setShowPassword(!showPassword)}>
+            <Image
+              source={showPassword ? icons.disable_eye : icons.eye}
+              style={{
+                height: 20,
+                width: 20,
+                tintColor: COLORS.white,
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+
+  function renderButton() {
+    return (
+      <View style={{margin: SIZES.padding * 3}}>
+        <TouchableOpacity
+          style={{
+            height: 60,
+            backgroundColor: COLORS.black,
+            borderRadius: SIZES.radius / 1.5,
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          onPress={() => console.log('Navigation to Home')}
+          >
+            <Text style={{ color: COLORS.white, /* ...FONTS.h3 */ }}>
+              Continue
+            </Text>
+          </TouchableOpacity>
+      </View>
+    );
   }
 
   return (
@@ -109,6 +259,7 @@ const SignUp = () => {
           {renderHeader()}
           {renderLogo()}
           {renderForm()}
+          {renderButton()}
         </ScrollView>
       </LinearGradient>
     </KeyboardAvoidingView>
